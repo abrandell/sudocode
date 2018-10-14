622b3d91d50d903f9ee101bf4a4163c19c40dbb0
adbrand <37308440+abrandell@users.noreply.github.com>
2018-10-14 06:24:03 -0700

    fixed type for serialVersionUID in UserNotLoggedInException

--- src/main/java/org/sudocode/api/user/UserNotLoggedInException.java
+++ src/main/java/org/sudocode/api/user/UserNotLoggedInException.java
@@ -2,7 +2,7 @@ package org.sudocode.api.user;
 
 public class UserNotLoggedInException extends RuntimeException {
 
-    private static final long serialversionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public UserNotLoggedInException() {
         super("User is not currently logged in");
