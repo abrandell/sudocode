c5623d9c7ca547fe9f70404131a6b61d1d980b51
adbrand <37308440+abrandell@users.noreply.github.com>
2018-10-16 11:13:35 -0700

    removed comment service and controller

--- src/main/java/org/sudocode/api/project/comment/CommentRestController.java
+++
@@ -1,9 +0,0 @@
-package org.sudocode.api.project.comment;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/projects/{projectId}/comments")
-public class CommentRestController {
-}
