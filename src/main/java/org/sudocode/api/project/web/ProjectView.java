5d614bab12fc97475d1716573df34528d8be8642
adbrand <37308440+abrandell@users.noreply.github.com>
2018-10-27 00:54:37 -0700

    added ProjectView projection interface

--- src/main/java/org/sudocode/api/project/web/ProjectView.java
+++ src/main/java/org/sudocode/api/project/web/ProjectView.java
@@ -1,4 +1,18 @@
 package org.sudocode.api.project.web;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.sudocode.api.project.Difficulty;
+import org.sudocode.api.user.web.UserView;
+
+import java.time.LocalDateTime;
+
+@JsonPropertyOrder({"id", "title", "difficulty", "description", "date_posted", "author"})
 public interface ProjectView {
+
+    Long getId();
+    String getTitle();
+    Difficulty getDifficulty();
+    String getDescription();
+    LocalDateTime getDatePosted();
+    UserView getAuthor();
 }
