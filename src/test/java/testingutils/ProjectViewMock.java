60c1b89dcc908366879049ef95a5039fee361937
adbrand <37308440+abrandell@users.noreply.github.com>
2018-10-28 11:08:13 -0700

    Added a ProjectViewMock to help with testing

---
+++ src/test/java/testingutils/ProjectViewMock.java
@@ -0,0 +1,24 @@
+package testingutils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.sudocode.api.post.project.Difficulty;
+import org.sudocode.api.post.project.ProjectView;
+import org.sudocode.api.user.UserView;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ProjectViewMock implements ProjectView {
+
+    Long id;
+    String title;
+    Difficulty difficulty;
+    String description;
+    LocalDateTime datePosted;
+    UserView author;
+
+}
